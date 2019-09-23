00c02de021ebeed4f82b5bbb2ed92fcb6d438d7a
unknown <gustavo102699>
2019-09-22 19:29:02 -0500

    Entidades cambiadas-Gustavo

--- src/main/java/pe/edu/upc/entity/Producto.java
+++ src/main/java/pe/edu/upc/entity/Producto.java
@@ -1,5 +1,92 @@
 package pe.edu.upc.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+@Entity
+	@Table(name = "Producto ")
 public class Producto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idProducto;
+	
+	@Column(name = "NombreProducto", nullable = false, length = 50)
+	private String nombreProducto;
+	
+	@ManyToOne
+	@JoinColumn(name = "idCategoria", nullable = false)
+	private Categoria categoria;
+	
+	public Producto() {
+		super();
+		
+	}
+	
+
+	public Producto(int idProducto, String nombreProducto, Categoria categoria, Marca marca, int stock) {
+		super();
+		this.idProducto = idProducto;
+		this.nombreProducto = nombreProducto;
+		this.categoria = categoria;
+		this.marca = marca;
+		this.stock = stock;
+	}
+
+
+	public int getIdProducto() {
+		return idProducto;
+	}
+
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+
+	public void setNombreProducto(String nombreProducto) {
+		this.nombreProducto = nombreProducto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "idMarca", nullable = false)
+	private Marca marca;	
+	
+	@Column(name = "Stock", nullable = false, length = 20)
+	private int stock;
+	
+	
+	
 
 }
--- src/main/java/pe/edu/upc/entity/Usuario.java
+++ src/main/java/pe/edu/upc/entity/Usuario.java
@@ -1,5 +1,62 @@
 package pe.edu.upc.entity;
 
-public class Usuario {
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Usuarios")
+
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	private int idUsuario;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@MapsId  //de esa tabla iba a jalar redes
+	@JoinColumn(name = "idUsuario", nullable = false)  //interactuar entre los componentes
+	private Empleado empleado;
+	
+	@Column(name = "username", nullable = false, length = 30)
+	private String username;
+
+	@Column(name = "password", nullable = false, length = 80)
+	private String password;
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+	
 
 }
